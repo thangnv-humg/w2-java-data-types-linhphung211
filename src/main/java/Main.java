c503072b44ae2fda856d6380d3a2f03b28ce62a2
Linh Phùng <lphun@LENOVO.com>
2025-01-16 14:58:06 +0700

    Lam bai tap 16/01/2025

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
-
+        int myInt = 42;
+        double myDouble = 3.14;
+        String myString = "Hello, Java!";
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myString);
+        if (myInt > 10)
+        {
+            System.out.println("Greater than 10");
+        }
+        else
+        {
+            System.out.println("Less or equal than 10");
+        }
         // Ví dụ:
         // int myInt = 42;
         // double myDouble = 3.14;
